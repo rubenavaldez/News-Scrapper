8018f8cd18cb471b3a610b946f7293e8edc3f6af
Ruben Valdez <ruben.valdez7@gmail.com>
2019-09-06 21:13:23 -0700

    title scrapped success

--- server.js
+++ server.js
@@ -3,16 +3,17 @@ const handlebars = require("express-handlebars")
 const mongoose = require("mongoose");
 const axios = require("axios");
 const logger = require("morgan")
-
+const cheerio = require("cheerio");
+let result = {};
 // var db = require("./models")
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
 
-var app = express();
+const app = express();
 
-var PORT = 3000;
+const PORT = 3000;
 
 app.use(logger("dev"));
 
@@ -23,21 +24,24 @@ app.use(express.static("public"));
 
 app.get("/scrape", function(req, res){
     axios.get("https://www.space.com/news").then(function(response){
-    console.log(response.data)
-    // var $ = cheerio.load(response.data);
-
-    // $("article-name").each(function(i,element){
-    //     var result = {};
-    //     result.title = $(this)
-    //     .children("a")
-    //     .text();
-    //     result.link = $(this)
-    //     .children("a")
-    //     .attr("href");
-
-    // })
-
     // console.log(response.data)
+    
+    const $ = cheerio.load(response.data);
+    const title = $('.article-name')
+    console.log(title.text())
+    $(".search-result-news").each(function(i,element){
+        // result = {};
+        // result.title = $(this)
+        // .children("h3")
+        // .text()
+        result.summary = $(this)
+        .children(".synopsis")
+        .text()
+        
+
+    })
+
+    console.log(result)
 
 
 
