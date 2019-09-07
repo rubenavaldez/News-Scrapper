c24a61a4665101c577b1433fc8bb5f2599ff7779
Ruben Valdez <ruben.valdez7@gmail.com>
2019-09-07 10:09:24 -0700

    mongoose model article working

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
-const handlebars = require("express-handlebars")
+const handlebars = require("express-handlebars");
 const mongoose = require("mongoose");
 const axios = require("axios");
-const logger = require("morgan")
+const logger = require("morgan");
 const cheerio = require("cheerio");
-// let result = {};
-// var db = require("./models")
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+const db = require("./models");
+
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
 
@@ -17,78 +18,62 @@ const PORT = 3000;
 
 app.use(logger("dev"));
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-app.get("/scrape", function(req, res){
-    axios.get("https://www.space.com/news").then(function(response){
+app.get("/scrape", function(req, res) {
+  axios.get("https://www.space.com/news").then(function(response) {
     // console.log(response.data)
-    
+
     const $ = cheerio.load(response.data);
     // const title = $('.article-name')
     // console.log(title.text())
-    $("div.listingResult").each(function(i,element){
-        let result = {};
-        result.title = $(this)
+    $("div.listingResult").each(function(i, element) {
+      let result = {};
+      result.title = $(this)
         .children("a")
-        .children('article')
-        .children('.content')
-        .children('header')
-        .children('h3')
-        .text()
+        .children("article")
+        .children(".content")
+        .children("header")
+        .children("h3")
+        .text();
 
-        result.summary = $(this)
+      result.summary = $(this)
         .children("a")
-        .children('article')
-        .children('.content')
-        .children('.synopsis')
-        .text()
+        .children("article")
+        .children(".content")
+        .children(".synopsis")
+        .text();
 
-        result.url = $(this)
+      result.url = $(this)
         .children(".article-link")
-        .attr('href')
-        
+        .attr("href");
 
-        
-        console.log(result)
-        
-    })
-    
+      // console.log(result)
+
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    });
+
     // $(".article-name").each(function(i,element){
     //     let result = {};
     //     result.title = $(this)
     //     // .children("h3")
     //     .text()
-        
+
     //     console.log(result)
-        
+    res.send("Scrape Complete")
     // })
-
-    
-
-
-
-
-    });
+  });
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.listen(PORT, () => {
-    console.log("App is running on port " + PORT );
-})
+  console.log("App is running on port " + PORT);
+});
